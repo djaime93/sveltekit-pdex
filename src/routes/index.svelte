b277af6993d78c895428f180af16d68afdfcf9a4
David <djaime93@gmail.com>
2021-06-14 11:37:50 -0500

    Add fetch pokemon in api in home

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,22 @@
+<script context="module">
+    export async function load({page}) {
+        const url = `https://pokeapi.co/api/v2/pokemon?limit=150`;
+        const res = await fetch(url);
+        const data = await res.json();
+        const loadedPokemon = data.results.map((data, index) => {
+            return {
+                name: data.name,
+                id: data.id,
+                image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index+1}.png`,
+            };
+        });
+        return {props: {pokemon:loadedPokemon}}
+    };
+</script>
+
+<script>
+    export let pokemon
+    console.log(pokemon)
+</script>
+
 <h1>My Sveltekit Pokedex</h1>
